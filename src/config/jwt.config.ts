579dab5c2233bc5882f8d8833a19d3ef4fd6dcd7
SakibJaber <sakibjaber9921@gmail.com>
2025-11-16 01:01:33 +0600

    refactor: simplify expiresIn type casting in jwt configuration

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,10 +1,8 @@
 import { JwtModuleOptions } from '@nestjs/jwt';
-import { StringValue } from 'jsonwebtoken';
 
 export const jwtConfig = (): JwtModuleOptions => ({
   secret: process.env.JWT_SECRET,
   signOptions: {
-    expiresIn:
-      (process.env.JWT_EXPIRES_IN as StringValue) || ('15m' as StringValue),
+    expiresIn: (process.env.JWT_EXPIRES_IN || '15m') as any,
   },
 });
